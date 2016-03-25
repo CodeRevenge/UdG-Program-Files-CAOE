ba7bc214a9d8e3dfadfe179066ea9da0c56254be
CodeRevenge <mas.arley2009@hotmail.com>
2016-03-24 23:46:11 -0600

    Create pseudo Residuo.cpp

---
+++ pseudo Residuo.cpp
@@ -0,0 +1,15 @@
+Incluir bibliotecas
+
+Inicio
+
+   var entero divs = 8
+   var entero divd = 100
+   var entero result, resid
+
+   result <- divd / divs
+   resid <- divd mod divs
+
+   Mostrar result
+   Mostrar resid
+
+Fin

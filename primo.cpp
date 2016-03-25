fb4e89a741e365245162f92f07dc4da0980ee465
CodeRevenge <mas.arley2009@hotmail.com>
2016-03-25 01:06:34 -0600

    Update primo.cpp

--- primo.cpp
+++ primo.cpp
@@ -4,29 +4,44 @@ using namespace std;
 
 int main()
 {
-   int numero, a=0, i;  
+   int numero, divs=0, i;       
+   
+   /*La variable divs la usaremos para contar el numero
+   divisores del numero*/
    
    cout << "Programa para determinar si un numero es primo" << endl;
    cout<< "Ingrese el numero: ";
    cin >> numero;
    
-   numero = abs(numero);
+   numero = abs(numero); 
+   
+   /*Esta funcion nos permite utilizar negativos tambien,
+    Convirtiendolos en su absoluto*/
    
    for(i=1;i<(numero+1);i++)
-    {  
-         if(numero%i==0)
-        {  
-             a++;  
-        }  
-    }  
-    if(a!=2)
-    {  
-        cout<<"No es un numero primo";  
-    }
-    else
-    {  
-        cout<<"Es un numero primo";  
-    }
+   {  
+      if(numero%i==0)
+      {  
+         divs++;                 
+         
+         /*Si el numero es divisible entre i entonces 
+         divs sera aumentado en 1*/
+      }  
+   }  
+   
+   if(divs!=2)   
+   
+   /*Si divs es diferente a dos, que es el numero de
+    divisores maximos que puede tener un primo,
+    se determina que no es un primo*/
     
-    return 0;  
+   {  
+      cout<<"No es un numero primo";  
+   }
+   else
+   {  
+      cout<<"Es un numero primo";  
+   }
+    
+   return 0;  
 }  

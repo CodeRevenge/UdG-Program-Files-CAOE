3fc3d520010c82d7551855230986f37f46c6535f
CodeRevenge <mas.arley2009@hotmail.com>
2016-03-24 23:58:58 -0600

    Update par-impar.cpp

--- par-impar.cpp
+++ par-impar.cpp
@@ -7,18 +7,27 @@ int main()
     int numero, resul;
     
     cout << "Programa que te muestra si es un numero par o impar" << endl;
-    cout << "Introduce un numero: ";
-    cin >> numero;
+    cout << "Introduce un numero: ";            //Pide un numero cualquiera
+    cin >> numero;                              //Y se guarda en la variable numero
     
-    resul = numero % 2;
+    resul = numero % 2;                         //se determina el modulo de dos y se guarda en resul
     
-    if(resul==1)
+    /* la intencion de determinar el modulo
+       es saber si nos queda una unidad
+       lo que determina que numero es un numero
+       indivisible entre dos, lo que es
+       impar; en cambio si no nos queda ninguna 
+       unidad, se determina que es un numero 
+       divisible entre dos, lo que es par */
+    
+    
+    if(resul==1)                                    //Aqui nos sobra una unidad
     {
-        cout << "El numero es impar";
+        cout << "El numero es impar";               //Lo que significa numero impar
     }
-    else
+    else                                            //En otro caso no nos sobra ninguna unidad
     {
-        cout << "El numero es par";
+        cout << "El numero es par";                 //Lo que significa numero impar
     }
     
     return 0;
